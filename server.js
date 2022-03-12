be4a1e97f1e4a37f895b400be77b25595c43ef35
Naeem Ashraf <naeemashraf.tud@gmail.com>
2022-03-12 13:03:15 +0500

    app object
    Use the express function to create an express app object

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ var express = require('express')
 // This will give you access to the express module in your project.
 // Use the express function to create an express app object. 
 // This will always be the first step we'll take to use express in our project:
+var app = express();

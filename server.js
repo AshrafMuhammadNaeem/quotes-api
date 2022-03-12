2f16c5b2ad89881f6db06b886320e187136284df
Naeem Ashraf <naeemashraf.tud@gmail.com>
2022-03-12 13:50:37 +0500

    hard code an array of JSON objects

--- server.js
+++ server.js
@@ -4,6 +4,27 @@ var express = require('express')
 // Use the express function to create an express app object. 
 // This will always be the first step we'll take to use express in our project:
 var app = express();
+// here below will come hard coded data base for quotes
+var quotes = [
+    {
+        id: 1,
+        quote: "The best is yet to come",
+        author: "Unknown",
+        year: 2000
+    },
+    {
+        id: 2,
+        quote: "This is a quote",
+        author: "First Last",
+        year: 1930
+    },
+    {
+        id: 3,
+        quote: "This is another quote",
+        author: "First2 Last2",
+        year: 1910
+    }
+    ];
 // Create a variable to store the port # your server will run on.
 var port = 3000;
 // This is the port your server will use to listen for requests. 
@@ -25,3 +46,9 @@ app.get('/', function(request, response){
 })
 //  Now start the server with node server.js in terminal and then go on to localhost:3000
 // to see the message "Get request reeceived at '/"
+
+// Now we will the GET routes for our API with following endpoints:
+// GET/quotes GET/quotes/:id and POST/quotes
+// Let's start with the GET /quotes route, which returns a list of all quotes.
+// For now, since we aren't yet connected to a database, let's hard code an array of JSON objects in our server file. 
+// Add this below the 2 import statements at the top of your server.js file:
